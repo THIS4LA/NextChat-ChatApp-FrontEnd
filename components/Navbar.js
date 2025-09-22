f9ba3ebc65ee9c3e0a4cac8a981dc7d8e4b34902
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-09-22 09:04:54 +0530

    update navbar with logo and user

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div>
-        <div className='bg-white text-black h-11'>
-            <h1 className=''>Navbar</h1>
-        </div>
+      <div className="bg-black w-full h-[54px] flex items-center justify-between px-[16px]">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="My Logo"
+            className="h-8 w-auto"
+            width={120}
+            height={40}
+            priority
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            src="/user.jpg"
+            alt="My Logo"
+             className="rounded-full w-[40px] h-[40px] object-cover"
+            width={120}
+            height={40}
+            priority
+          />
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
