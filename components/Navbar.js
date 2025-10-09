3f9dccd69d533a0d042cc6291d4988773f34a542
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-10-09 12:35:53 +0530

    update navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,29 +4,29 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="bg-black w-full h-[54px] flex items-center justify-between px-[16px]">
-        <Link href="/">
+    <div className="bg-black w-full h-[58px] flex items-center justify-between px-[8px]">
+      <Link href="/">
+        <div className="relative h-10 w-32">
           <Image
             src="/logo.png"
             alt="My Logo"
-            className="h-8 w-auto"
-            width={120}
-            height={40}
+            fill
+            className="object-contain"
             priority
+            sizes="(max-width: 768px) 100px, (max-width: 1200px) 128px, 128px"
           />
-        </Link>
-        <Link href="/">
-          <Image
-            src="/user.jpg"
-            alt="My Logo"
-             className="rounded-full w-[40px] h-[40px] object-cover"
-            width={120}
-            height={40}
-            priority
-          />
-        </Link>
-      </div>
+        </div>
+      </Link>
+      <Link href="/">
+        <Image
+          src="/user.jpg"
+          alt="User Avatar"
+          className="rounded-full w-10 h-10 object-cover"
+          width={40}
+          height={40}
+          priority
+        />
+      </Link>
     </div>
   );
 };
