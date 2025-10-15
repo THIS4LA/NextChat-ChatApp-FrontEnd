a5710ee65c735769ad896cd53b3274cab2e9ba4b
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-10-15 23:10:55 +0530

    add profile pic add func

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -4,8 +4,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { registerUser } from "../../../store/authSlice";
 import Image from "next/image";
 import Link from "next/link";
+import { IoCloudUpload } from "react-icons/io5";
+import { ScaleLoader } from "react-spinners";
 
 const Register = () => {
+  const [url, setUrl] = useState("");
+  const [imgUploading, setImgUploading] = useState(false);
   const dispatch = useDispatch();
   const { user, loading, error } = useSelector((state) => state.auth);
   const [form, setForm] = useState({
@@ -14,6 +18,33 @@ const Register = () => {
     password: "",
     avatar: "",
   });
+
+  async function handleImgChange(e) {
+    const file = e.target.files[0];
+    setImgUploading(true);
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", process.env.NEXT_PUBLIC_UPLOAD_PRESET);
+
+    try {
+      const res = await fetch(
+        `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUD_NAME}/image/upload`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      const data = await res.json();
+      setUrl(data.secure_url);
+      setForm((prev) => ({ ...prev, avatar: data.secure_url }));
+    } catch (err) {
+      console.error("Upload error:", err);
+    } finally {
+      setImgUploading(false);
+    }
+  }
+
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
@@ -26,7 +57,9 @@ const Register = () => {
     <div className="flex items-center justify-center h-screen">
       <div className="max-w-md w-full mx-auto p-6 border rounded">
         <h1 className="text-3xl mb-1 flex justify-center">Register</h1>
-        <p className="mb-8 text-center text-gray-400">Let&apos;s Get You Started !</p>
+        <p className="mb-8 text-center text-gray-400">
+          Let&apos;s Get You Started !
+        </p>
         <p className="mb-8 text-center text-gray-400">
           {error && <span className="text-red-500">{error.message}</span>}
           {user && <span className="text-green-500">Register Successful!</span>}
@@ -62,24 +95,65 @@ const Register = () => {
             required
             className="border p-2"
           />
-          <input
-            type="text"
-            name="avatar"
-            placeholder="Avatar URL"
-            value={form.avatar}
-            onChange={handleChange}
-            disabled={loading}
-            required
-            className="border p-2"
-          />
+          <div className="w-full h-40 border border-dashed rounded overflow-hidden relative">
+            {imgUploading ? (
+              <div className="flex justify-center items-center h-full w-full">
+                <ScaleLoader color="#7BF1A8" height={18} />
+              </div>
+            ) : form.avatar ? (
+              <>
+                <Image
+                  unoptimized
+                  src={url}
+                  alt="avatar"
+                  fill
+                  className="object-cover"
+                />
+                <button
+                  type="button"
+                  onClick={() => {
+                    setUrl("");
+                    setForm((prev) => ({ ...prev, avatar: "" }));
+                  }}
+                  className="absolute top-2 right-2 bg-red-500 text-white text-xs px-2 py-1 rounded hover:cursor-pointer"
+                >
+                  ✕
+                </button>
+              </>
+            ) : (
+              <label
+                htmlFor="fileUpload"
+                className="w-full h-full flex justify-center items-center cursor-pointer"
+              >
+                <div className="flex flex-col items-center text-gray-500 hover:text-green-300">
+                  <IoCloudUpload className="text-3xl mb-1" />
+                  <span className="text-sm font-medium">Upload an image</span>
+                </div>
+                <input
+                  id="fileUpload"
+                  type="file"
+                  name="img"
+                  onChange={handleImgChange}
+                  className="hidden"
+                  required
+                />
+              </label>
+            )}
+          </div>
+
           <button
             type="submit"
             className="bg-green-300 p-2 hover:bg-green-400 text-black"
-            disabled={loading}
+            disabled={loading || imgUploading}
           >
             Register
           </button>
-          <p className="text-sm text-center text-gray-400">Already have an account? <span className="text-green-400 ml-1 cursor-pointer hover:text-green-600"><Link href="/login">Sign here</Link></span></p>
+          <p className="text-sm text-center text-gray-400">
+            Already have an account?{" "}
+            <span className="text-green-400 ml-1 cursor-pointer hover:text-green-600">
+              <Link href="/login">Sign here</Link>
+            </span>
+          </p>
         </form>
       </div>
     </div>
