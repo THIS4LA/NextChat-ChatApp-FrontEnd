defbdd8502f3e2760fd2494fe9ea85a9307b33a6
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-10-17 23:29:20 +0530

    fix chat page didn't open in mobile mode

--- src/app/messages/layout.js
+++ src/app/messages/layout.js
@@ -1,9 +1,13 @@
 "use client";
 import RecentMessages from "./page.js";
+import { usePathname } from "next/navigation";
 import Chat from "./[id]/page.js";
 import ProtectedRoute from "../../../components/ProtectedRoute.js";
 
 export default function MessagesLayout() {
+  const pathname = usePathname();
+  // check if current path includes an id
+  const isChatOpen = /^\/messages\/[^/]+$/.test(pathname);
   return (
     <ProtectedRoute>
       <div className="w-full h-screen">
@@ -18,7 +22,7 @@ export default function MessagesLayout() {
         </div>
 
         <div className="md:hidden w-full h-full">
-          <RecentMessages />
+          {!isChatOpen ? <RecentMessages /> : <Chat />}
         </div>
       </div>
     </ProtectedRoute>
