82cf78f735e937119776d299d33b3e0b5b186a1a
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-10-22 10:52:40 +0530

    create a singleton instance for avoid socket duplication

--- socket/socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-const socket = io(process.env.NEXT_PUBLIC_API_URL, {
-  transports: ["websocket"], 
-  withCredentials: true,
-});
-
-export default socket;
