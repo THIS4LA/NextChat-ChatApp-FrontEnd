85ba56904731c0e8a595a884273873a247f8f011
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-11-03 22:46:02 +0530

    fix infinite redirect loop after build

--- components/ProtectedRoute.js
+++ components/ProtectedRoute.js
@@ -1,16 +1,25 @@
 "use client";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ScaleLoader } from "react-spinners";
 
 export default function ProtectedRoute({ children }) {
   const router = useRouter();
   const token = useSelector((state) => state.auth.token);
+  const [isChecking, setIsChecking] = useState(true);
 
   useEffect(() => {
-    if (!token) router.replace("/");
+    // Simulate waiting for Redux hydration
+    if (typeof token === "undefined") return;
+
+    if (!token) {
+      router.replace("/");
+    } else {
+      setIsChecking(false);
+    }
   }, [token, router]);
 
-  if (!token) return null;
+  if (isChecking) return <ScaleLoader color="#00B879" />;
   return children;
 }
